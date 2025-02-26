f4fd27bb9df3aa06491322112c4c125132f5981e
João Marcelo Carvalho Pereira Araújo <joaomarcelocpa0303@gmail.com>
2025-02-25 23:56:14 -0300

    Add files via upload

---
+++ Aula2_NivelamentoPOO/src/App.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Digite a descrição do produto:");
+        String descricao = scanner.nextLine();
+
+        System.out.println("Digite o preço de custo do produto:");
+        double precoCusto = scanner.nextDouble();
+
+        System.out.println("Digite a margem de lucro do produto:");
+        double margemLucro = scanner.nextDouble();
+
+        System.out.println("O produto é perecível? (s/n):");
+        char perecivel = scanner.next().charAt(0);
+
+        if (perecivel == 's') {
+            System.out.println("Digite a data de validade do produto (dd/MM/yyyy):");
+            String dataValidadeStr = scanner.next();
+            LocalDate dataValidade = LocalDate.parse(dataValidadeStr, formatter);
+
+            try {
+                ProdutoPerecivel produtoPerecivel = new ProdutoPerecivel(descricao, precoCusto, margemLucro, dataValidade);
+                System.out.println(produtoPerecivel);
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        else if (perecivel == 'n') {
+            ProdutoNaoPerecivel produtoNaoPerecivel = new ProdutoNaoPerecivel(descricao, precoCusto, margemLucro);
+            System.out.println(produtoNaoPerecivel);
+        } else {
+            System.err.println("Opção inválida.");
+        }
+        scanner.close();
+    }
+}
