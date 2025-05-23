e2cd15976cdd8272dd78265fa3a42bbaf19cac85
João Marcelo Carvalho Pereira Araújo <joaomarcelocpa0303@gmail.com>
2025-05-23 15:51:02 -0300

    Delete AtividadesTAD's directory

--- AtividadesTAD's/FilasCelula/src/Celula.java
+++
@@ -1,9 +0,0 @@
-public class Celula {
-    public int elemento;
-    public Celula proximo;
-
-    public Celula(int elemento) {
-        this.elemento = elemento;
-        this.proximo = null;
-    }
-}
