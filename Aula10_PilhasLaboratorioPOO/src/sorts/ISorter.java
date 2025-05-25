aab46d1d67c2f2cbcc942fbac531921c67a4b8ac
João Marcelo Carvalho Pereira Araújo <joaomarcelocpa0303@gmail.com>
2025-05-25 11:41:46 -0300

    Delete Aula10_PilhasLaboratorioPOO directory

--- Aula10_PilhasLaboratorioPOO/src/sorts/ISorter.java
+++
@@ -1,9 +0,0 @@
-package sorts;
-
-import java.util.Comparator;
-
-public interface ISorter<T extends Comparable<T>> {
-
-    T[] sort(T[] array);
-    T[] sort(T[] array, Comparator<T> comparator);
-}
